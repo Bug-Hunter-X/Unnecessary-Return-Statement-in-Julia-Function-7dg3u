76ed0d815acf67c92c95177b0e939b61cf713a62
BugHunter-X <askzenai@gmail.com>
2025-02-16 01:18:04 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,17 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  elseif x == 0
+    return 0
+  else
+    return -x^2  #this line is wrong
+  end
+  #this line should never run
+  return 1
+ end
+
+println(myfunction(1))
+println(myfunction(0))
+println(myfunction(-1)) 
+```
